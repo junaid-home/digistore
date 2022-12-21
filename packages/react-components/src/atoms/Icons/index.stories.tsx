bd9babc0548c041ce9da3857a8398cbd716ddf77
junaid-home <jj123dev@gmail.com>
2022-12-21 19:33:08 +0500

    feat(ui): add search icon atomic component

--- packages/react-components/src/atoms/Icons/index.stories.tsx
+++ packages/react-components/src/atoms/Icons/index.stories.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { withA11y } from "@storybook/addon-a11y";
 
-import { ArrowDownIcon, LogoIcon, ShopIcon } from "./index";
+import { ArrowDownIcon, LogoIcon, ShopIcon, SearchIcon } from "./index";
 
 export const Logo = (args: any) => <LogoIcon {...args} />;
 export const Shop = (args: any) => <ShopIcon {...args} />;
 export const ArrowDown = (args: any) => <ArrowDownIcon {...args} />;
+export const Search = (args: any) => <SearchIcon {...args} />;
 
 ArrowDown.argTypes = {
   color: {
@@ -14,8 +15,18 @@ ArrowDown.argTypes = {
     },
   },
 };
-
 ArrowDown.args = {
+  color: "#333",
+};
+
+Search.argTypes = {
+  color: {
+    control: {
+      type: "text",
+    },
+  },
+};
+Search.args = {
   color: "#333",
 };
 
--- packages/react-components/src/atoms/Icons/index.ts
+++ packages/react-components/src/atoms/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as LogoIcon } from "./logo";
 export { default as ShopIcon } from "./shop";
+export { default as SearchIcon } from "./search";
 export { default as ArrowDownIcon } from "./arrow-down";
