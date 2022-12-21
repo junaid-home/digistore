bec1b1e4af8fe87f8fba7b4a9ad8b4a3e9ba3235
junaid-home <jj123dev@gmail.com>
2022-12-21 19:20:17 +0500

    refactor(ui): export all atomic icons from single index.ts file

--- packages/react-components/src/atoms/Icons/index.stories.tsx
+++ packages/react-components/src/atoms/Icons/index.stories.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { withA11y } from "@storybook/addon-a11y";
 
-import LogoIcon from "./logo";
-import ShopIcon from "./shop";
-import ArrowDownIcon from "./arrow-down";
+import { ArrowDownIcon, LogoIcon, ShopIcon } from "./index";
 
 export const Logo = (args: any) => <LogoIcon {...args} />;
 export const Shop = (args: any) => <ShopIcon {...args} />;
---
+++ packages/react-components/src/atoms/Icons/index.ts
@@ -0,0 +1,3 @@
+export { default as LogoIcon } from "./logo";
+export { default as ShopIcon } from "./shop";
+export { default as ArrowDownIcon } from "./arrow-down";
