66ad6fe994c39c6d56feeee46df22f69cafb3f1e
junaid-home <jj123dev@gmail.com>
2022-12-21 19:05:33 +0500

    feat(ui): add shop & arrow-down icon atomic components

--- packages/react-components/src/atoms/Icons/index.stories.tsx
+++ packages/react-components/src/atoms/Icons/index.stories.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import { withA11y } from "@storybook/addon-a11y";
 
 import LogoIcon from "./logo";
+import ShopIcon from "./shop";
+import ArrowDownIcon from "./arrow-down";
 
 export const Logo = (args: any) => <LogoIcon {...args} />;
+export const Shop = (args: any) => <ShopIcon {...args} />;
+export const ArrowDown = (args: any) => <ArrowDownIcon {...args} />;
+
+ArrowDown.argTypes = {
+  color: {
+    control: {
+      type: "text",
+    },
+  },
+};
+
+ArrowDown.args = {
+  color: "#333",
+};
 
 export default {
   title: "Atoms/Icons",
