e397609614c2aea5f90142222b2e2230410655ff
junaid-home <jj123dev@gmail.com>
2022-12-22 18:58:48 +0500

    feat(ui): add like, menu, cart & user icon atomic components

--- packages/react-components/src/atoms/Icons/index.stories.tsx
+++ packages/react-components/src/atoms/Icons/index.stories.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { withA11y } from "@storybook/addon-a11y";
 
-import { ArrowDownIcon, LogoIcon, ShopIcon, SearchIcon } from "./index";
+import {
+  ArrowDownIcon,
+  LogoIcon,
+  ShopIcon,
+  SearchIcon,
+  LikeIcon,
+  CartIcon,
+  UserIcon,
+  MenuIcon,
+} from "./index";
 
 export const Logo = (args: any) => <LogoIcon {...args} />;
 export const Shop = (args: any) => <ShopIcon {...args} />;
 export const ArrowDown = (args: any) => <ArrowDownIcon {...args} />;
 export const Search = (args: any) => <SearchIcon {...args} />;
+export const User = (args: any) => <UserIcon {...args} />;
+export const Menu = (args: any) => <MenuIcon {...args} />;
+export const Like = (args: any) => <LikeIcon totalCount={10} {...args} />;
+export const Cart = (args: any) => <CartIcon totalCount={0} {...args} />;
 
 ArrowDown.argTypes = {
   color: {
--- packages/react-components/src/atoms/Icons/index.ts
+++ packages/react-components/src/atoms/Icons/index.ts
@@ -2,3 +2,7 @@ export { default as LogoIcon } from "./logo";
 export { default as ShopIcon } from "./shop";
 export { default as SearchIcon } from "./search";
 export { default as ArrowDownIcon } from "./arrow-down";
+export { default as LikeIcon } from "./like";
+export { default as CartIcon } from "./cart";
+export { default as UserIcon } from "./user";
+export { default as MenuIcon } from "./menu";
