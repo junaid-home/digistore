373ed8f61a09d265303ff3a129b23805ad9f4d00
junaid-home <jj123dev@gmail.com>
2022-12-23 12:00:42 +0500

    refector(ui): export icons from **atoms/index.ts** file

--- packages/react-components/src/atoms/index.ts
+++ packages/react-components/src/atoms/index.ts
@@ -1,2 +1,4 @@
+export * from "./Icons";
 export { default as Button } from "./Button/button";
 export { default as Typography } from "./Typography/typography";
+export { default as Input } from "./Input/input";
