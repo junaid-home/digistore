9879c0b9429bf08c18b1a2b9b38b460a08af7a4d
junaid-home <jj123dev@gmail.com>
2023-01-25 16:18:42 +0500

    style(ui): extract inline styles to seperate file

--- packages/react-components/src/molecules/Avatar/avatar.tsx
+++ packages/react-components/src/molecules/Avatar/avatar.tsx
@@ -1,3 +1,5 @@
+import cls from "@digistore/scss/lib/molecules/Avatar.module.css";
+
 import * as React from "react";
 
 import { UserIcon } from "../../atoms/Icons";
@@ -6,7 +8,7 @@ import StackedText from "../Stacked-text/stacked-text";
 
 function Avatar({ user }: { user?: object }) {
   return (
-    <div style={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+    <div className={cls.wrapper}>
       <UserIcon />
       <StackedText className="lm-sm" label="Sign In" value="Account" />
     </div>
